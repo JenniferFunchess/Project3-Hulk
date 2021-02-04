e602d074bef0bb4c5fa8a248a60a69d3ef251219
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2021-02-03 21:24:56 -0500

    added footer component

---
+++ client/src/components/Footer/footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './style';
+
+function footer() {
+    return (
+        <footer class="page-footer">
+            <div class="footer-copyright">
+            <div class="container">
+            On-Track App 2021
+            {/* <a class="grey-text text-lighten-4 right" href="#!">More Links</a> */}
+            </div>
+            </div>
+      </footer>
+    );
+}
+
+export default footer;

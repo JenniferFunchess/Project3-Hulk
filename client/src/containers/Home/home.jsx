ecc79ce1aed12bce48214f0631ef10561474c352
Jennifer Funchess <jfunchess@ymail.com>
2021-02-05 23:08:09 -0500

    updated app.js links

--- client/src/containers/Home/home.jsx
+++ client/src/containers/Home/home.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
+import htmlIcon from "../../images/html-icon.png";
 
 const home = () => {
     return (
-        <>
-            <div class="row">
-            <div class="col s6"><img id ="school" src="./icons/school.png"></div>
-            <div class="col s6" id ="homeRight">
-                <div class="row" id = "homeStars">
-                    <img id ="stars" src="./icons/star-45px.png">
-                    <img id ="stars" src="./icons/star-45px.png">
-                    <img id ="stars" src="./icons/star-45px.png">
-                    <img id ="stars" src="./icons/star-45px.png">
-                    <img id ="stars" src="./icons/star-45px.png">
+        <div className= "container">
+            <div className="row">
+            <div className="col s6"><img src={htmlIcon} class="school" alt="yellow school"/>
+            <div className="col s6" id ="homeRight">
+                <div className="row" id = "homeStars">
+                    <img src={htmlIcon} class="stars" alt="gold star"/>
+                    <img src={htmlIcon} class="stars" alt="gold star"/>
+                    <img src={htmlIcon} class="stars" alt="gold star"/>
+                    <img src={htmlIcon} class="stars" alt="gold star"/>
+                    <img src={htmlIcon} class="stars" alt="gold star"/>
                 </div>
-                <div class="row" id="homeName">
+                <div className="row" id="homeName">
                     ON TRACK APP
                 </div>
-                <div class="row" id="rowTagline">
+                <div className="row" id="rowTagline">
                     REWARD STUDENTS FOR POSITIVE BEHAVIOR IN CLASS
                 </div>
-                <div class="row" id="signUpButton">
-                    <a class="waves-effect red darken-1 btn">SIGN UP</a>
+                <div className="row" id="signUpButton">
+                    <a className="waves-effect red darken-1 btn">SIGN UP</a>
                 </div>
             </div>
-          </div>
-        <>
+        </div>
+        </div>
+        </div>
     );
 };
 

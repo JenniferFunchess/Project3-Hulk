a24611badc0e2f5ab66b9941b6bcc841b2c03a67
Jennifer Funchess <jfunchess@ymail.com>
2021-02-05 23:10:48 -0500

    updated signup route in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
 import Home from "./containers/Home/home";
+import signup from "./containers/Signup/signup";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Switch> 
          <Route exact path="/" component={Home} />
+         <Route exact path="/signup" component={signup} />
         </Switch>
       </Router>
     </div>

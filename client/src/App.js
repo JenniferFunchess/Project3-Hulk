f2a1a9af5f2679c236ae69c950afd814b19ceb57
Austin Ellis <Austin@Janets-iMac.local>
2021-02-10 02:02:22 -0500

    Added some cards to class-list.jsx and teacher-home.jsx with Links to pages

--- client/src/App.js
+++ client/src/App.js
@@ -7,23 +7,28 @@ import StudentLogin from "./containers/Login/StudentLogin/studentlogin";
 import AddStudent from "./containers/Add-Student/add-student";
 import ViewStudent from "./containers/ViewStudent/viewStudent";
 import Rewards from "./containers/Rewards/rewards";
+import ViewList from "./containers/Class-List/class-list";
+import TeacherHome from "./containers/Teacher-Home/teacher-home";
 
 function App() {
   return (
     <div className="App">
       <Router>
-      <Navbar login={false} />
-        <Switch> 
-         <Route exact path="/" component={home} />
-         <Route exact path="/signup" component={signup} />
-         <Route exact path="/teacherlogin" component={TeacherLogin} />
-         <Route exact path="/studentlogin" component={StudentLogin} />
+        <Navbar login={false} />
+        <Switch>
+          <Route exact path="/" component={home} />
+          <Route exact path="/signup" component={signup} />
+          <Route exact path="/teacherlogin" component={TeacherLogin} />
+          <Route exact path="/studentlogin" component={StudentLogin} />
+          <Route exact path="/teacherhome" component={TeacherHome} />
+          <Route exact path="/classlist" component={ViewList} />
           <Route path="/add-student" component={AddStudent} />
-          <Route path="/view-student" component={ViewStudent}/>
-          <Route path="/rewards" component={Rewards}/>
+          <Route path="/view-student" component={ViewStudent} />
+          <Route path="/rewards" component={Rewards} />
         </Switch>
       </Router>
     </div>
   );
 }
 export default App;
+
--- client/src/containers/Class-List/class-list.jsx
+++ client/src/containers/Class-List/class-list.jsx
@@ -1,12 +1,130 @@
-import React from 'react';
+import React from "react";
 import "./class-list.css";
+import StudentProfile from "../../images/school.png";
+import { Link } from "react-router-dom";
 
 const ViewClass = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+          <h1>Class List</h1>
         </div>
-    );
+        <div className="row">
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img
+                  src={StudentProfile}
+                  alt="Student Profile Picture"
+                  style={{ width: "100%" }}
+                />
+                <span className="card-title">Title</span>
+                <Link to="/view-student" className="btn-floating halfway-fab waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </Link>
+              </div>
+              <div className="card-content">
+                <p className="student-name">Student Name</p>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img
+                  src={StudentProfile}
+                  alt="Student Profile Picture"
+                  style={{ width: "100%" }}
+                />
+                <span className="card-title">Title</span>
+                <Link to="/view-student" className="btn-floating halfway-fab waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </Link>
+              </div>
+              <div className="card-content">
+                <p className="student-name">Student Name</p>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img
+                  src={StudentProfile}
+                  alt="Student Profile Picture"
+                  style={{ width: "100%" }}
+                />
+                <span className="card-title">Title</span>
+                <Link to="/view-student" className="btn-floating halfway-fab waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </Link>
+              </div>
+              <div className="card-content">
+                <p className="student-name">Student Name</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img
+                  src={StudentProfile}
+                  alt="Student Profile Picture"
+                  style={{ width: "100%" }}
+                />
+                <span className="card-title">Title</span>
+                <Link to="/view-student" className="btn-floating halfway-fab waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </Link>
+              </div>
+              <div className="card-content">
+                <p className="student-name">Student Name</p>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img
+                  src={StudentProfile}
+                  alt="Student Profile Picture"
+                  style={{ width: "100%" }}
+                />
+                <span className="card-title">Title</span>
+                <Link to="/view-student" className="btn-floating halfway-fab waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </Link>
+              </div>
+              <div className="card-content">
+                <p className="student-name">Student Name</p>
+              </div>
+            </div>
+          </div>
+          <div className="col s12 m4">
+            <div className="card">
+              <div className="card-image">
+                <img
+                  src={StudentProfile}
+                  alt="Student Profile Picture"
+                  style={{ width: "100%" }}
+                />
+                <span className="card-title">Title</span>
+                <Link to="/view-student" className="btn-floating halfway-fab waves-effect waves-light red">
+                  <i className="material-icons">add</i>
+                </Link>
+              </div>
+              <div className="card-content">
+                <p className="student-name">Student Name</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ViewClass;

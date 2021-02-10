bd25d5423eca9d596f17e8a0968fa905e62a242c
Austin Ellis <Austin@Janets-iMac.local>
2021-02-09 20:35:59 -0500

    Still working on class list

---
+++ client/src/containers/Class-List/class-list.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./class-list.css";
+
+const ViewClass = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ViewClass;

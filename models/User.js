30dfda12d4e2120a907becaaf021c9b0044676a1
Jennifer Funchess <jfunchess@ymail.com>
2021-02-17 19:43:26 -0600

    updated user model

--- models/User.js
+++ models/User.js
@@ -3,16 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  firstName: {
-    type: String,
-    trim: true,
-    required: "Enter your First Name.",
-  },
-  lastName: {
-    type: String,
-    trim: true,
-    required: "Enter your Last Name.",
-  },
+
   email: {
     type: String,
     trim: true,
@@ -22,11 +13,6 @@ const userSchema = new Schema({
     type: String,
     trim: true,
     required: "Enter a password",
-  },
-  teacherNickname: {
-    type: String,
-    trim: true,
-    required: "Enter a Teacher Nickname.",
   },
 });
 userSchema.pre("save", function (next) {
